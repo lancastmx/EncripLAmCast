1773b16af8519ff5020017dc3341d9daad4be79c
lancast <angelprep92@gmail.com>
2023-01-13 06:24:16 -0600

    Funciona los botones encriptar,desencriptar y copiar. Fata poner las alarmas

---
+++ page/js/lancastEncrip.js
@@ -0,0 +1,86 @@
+
+// Comenta en javaSript
+function encriptar() {
+    let texto = document.getElementById("input-texto").value;
+    let txt_cifrado = texto.replace(/e/gm, "enter");
+    txt_cifrado = txt_cifrado.replace(/o/gm, "ober");
+    txt_cifrado = txt_cifrado.replace(/i/gm, "imes");
+    txt_cifrado = txt_cifrado.replace(/a/gm, "ai");
+    txt_cifrado = txt_cifrado.replace(/u/gm, "ufat");
+
+    if (/[^a-zñ ]/.test(texto)) {
+        Swal.fire({
+            icon: 'error',
+            iconColor: '#b9ab9c',
+            background: '#E3E0DE',
+            title: 'Oops...',
+            confirmButtonColor: '#b9ab9c',
+            text: 'Solo se permiten letras minusculas y sin acentos',
+          }
+    }
+    /**else if (texto.length === 0) {
+        Swal.fire({
+            icon: 'error',
+            iconColor: '#b9ab9c',
+            background: '#E3E0DE',
+            title: 'Oops...',
+            confirmButtonColor: '#b9ab9c',
+            text: 'El campo de texto está vacio',
+          });
+    }
+    else {
+        document.getElementById("texto1-contder").style.display = "none";
+        document.getElementById("texto2-contder").style.display = "none";
+        document.getElementById("output-texto").style.display = "inline-block";
+        document.getElementById("output-texto").innerHTML = txt_cifrado;
+        
+        value(); 
+    } */
+    
+}
+
+function desencriptar() {
+    let texto = document.getElementById("input-texto").value;
+    let txt_cifrado = texto.replace(/enter/gm, "e");
+    txt_cifrado = txt_cifrado.replace(/ober/gm, "o");
+    txt_cifrado = txt_cifrado.replace(/imes/gm, "i");
+    txt_cifrado = txt_cifrado.replace(/ai/gm, "a");
+    txt_cifrado = txt_cifrado.replace(/ufat/gm, "u");
+
+    if (texto.length === 0) {
+        Swal.fire({
+            icon: 'error',
+            iconColor: '#b9ab9c',
+            background: '#E3E0DE',
+            title: 'Oops...',
+            confirmButtonColor: '#b9ab9c',
+            text: 'El campo de texto está vacio',
+        });
+    }
+/*
+    else {
+    document.getElementById("texto1-contder").style.display = "none";
+    document.getElementById("texto2-contder").style.display = "none";
+    document.getElementById("output-texto").style.display = "inline-block";
+    document.getElementById("output-texto").innerHTML = txt_cifrado;
+
+    value();    
+    }
+} */ 
+
+function copiar() {
+    let contenido = document.querySelector("#output-texto");
+    contenido.select();
+    document.execCommand("copy");
+    Swal.fire({
+        icon: 'success',
+        iconColor: '#b9ab9c',
+        background: '#E3E0DE',
+        title: '¡Bien!',
+        confirmButtonColor: '#b9ab9c',
+        text: 'Texto copiado correctamente',
+    });
+}
+
+focus();
+value();
